6670badf108ba08b73433774bac43ed0a7acaa2c
mursaleen <mursaleenarshad65@gmail.com>
2025-03-08 22:23:38 +0500

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question1
+{
+   
+       
+       
+
+class TripProgram
+    {
+        static void Main()
+        {
+            Trip trip = new Trip("Lahore", 500, 100, 20);
+
+            Console.WriteLine("Trip Destination: " + trip.Destination);
+            Console.WriteLine("Miles per gallon: " + trip.milespergallon());
+            Console.WriteLine("Cost per mile: " + trip.costpermile());
+        }
+    }
+
+
+
+
+} 
+
+ 
---
+++ Trip.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question1
+{
+    internal class Trip
+    {
+    
+        public string Destination;
+        public double distancetraveled;
+        public double totalcostgasoline;
+        public double gallonsconsumed;
+
+        public Trip(string destination, double distance, double cost, double gallons)
+        {
+            Destination = destination;
+            distancetraveled = distance;
+            totalcostgasoline = cost;
+            gallonsconsumed = gallons;
+        }
+
+        public double milespergallon()
+        {
+            return distancetraveled / gallonsconsumed;
+        }
+
+        public double costpermile()
+        {
+            return totalcostgasoline / distancetraveled;
+        }
+    }
+
+}
+
